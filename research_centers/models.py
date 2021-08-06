5821c96507b1a63cc8b6aad81bd336b88adb76a6
tushar <tushar19340@iiitd.ac.in>
2021-08-06 19:03:14 +0530

    Logo added

--- research_centers/models.py
+++ research_centers/models.py
@@ -5,10 +5,12 @@ from cloudinary.models import CloudinaryField
 
 class Research_Center(models.Model):
     name = models.CharField(max_length=250)
-    image1 = CloudinaryField('image1', help_text='<b style="color:green;font-size: 12px">*IMPORTANT* All three image should be of equal ratio (preffered: 3 x 2 )</b>')
+    logo = CloudinaryField('logo')
+    image1 = CloudinaryField('image1', blank=True, null=True , help_text='<b style="color:green;font-size: 15px">*IMPORTANT* All three image should be of equal ratio (preffered: 3 x 2 )</b>')
     image2 = CloudinaryField('image2', blank=True, null=True)
     image3 = CloudinaryField('image3', blank=True, null=True)
-    video = models.CharField("Embeded Url", max_length=500, blank=True, null=True)
+    join_us = models.CharField(max_length=1000, blank=True, null=True)
+    video = models.CharField("Embeded Url", max_length=500, blank=True, null=True, help_text='<b style="color:blue;font-size: 15px">Video that will we shown on join us card of center</b>')
     short_description = models.TextField(blank=True, null=True)
     info = RichTextField()
 
