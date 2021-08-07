92ece0a3a3a8dd215445f2789b4e8a3f35e93743
tushar <tushar19340@iiitd.ac.in>
2021-08-08 00:49:08 +0530

    minor changes

--- career_talks/models.py
+++ career_talks/models.py
@@ -14,12 +14,13 @@ class Category(models.Model):
     
 class Career_Talk(models.Model):
     title = models.CharField(max_length = 200)
+    short_description = models.TextField(max_length=250, blank=True, null=True, help_text='<b style="color:dodgerblue;font-size: 12px">Description of the card shown on all careers talk page. *MAX_Length = 250 characters"</b>')
     description = RichTextField("Description", null = True)
     image = CloudinaryField('image', blank=True, null=True)
-    embeded_video_link = models.TextField(blank=True, null=True, help_text='<b style="color:green;font-size: 12px">*IMPORTANT* Only add either Video link or Image, If you add both, only the image will be shown</b>')
+    embeded_video_link = models.TextField(blank=True, null=True, help_text='<b style="color:orange;font-size: 12px">*IMPORTANT* Only add either Video link or Image, If you add both, only the image will be shown</b>')
     date_added = models.DateTimeField(auto_now_add = True)
     category = models.ManyToManyField(Category, blank=True, null=True)
-    is_active = models.BooleanField(default = True)
+    status = models.BooleanField("Show", default = True)
 
     class Meta:
         verbose_name_plural = 'Career Talks'
